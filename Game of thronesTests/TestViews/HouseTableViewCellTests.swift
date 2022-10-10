0b646d23f67ac7043987b634ad9debe75407a303
Kuda <kudazata@gmail.com>
2022-10-10 12:22:16 +0200

    small bug fix

--- Game of thronesTests/TestViews/HouseTableViewCellTests.swift
+++ Game of thronesTests/TestViews/HouseTableViewCellTests.swift
@@ -51,11 +51,5 @@ class HouseTableViewCellTests: XCTestCase {
         tableView = nil
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
